ce6d0b27cbbe51b158065af2ba61376cf971020a
neelayam kandel <neelayamdottkandel@gmail.com>
2023-06-03 13:33:30 +0545

    perf: static method

--- AMS/src/com/view/main.java
+++ AMS/src/com/view/main.java
@@ -1,5 +1,46 @@
 package com.view;
 
 public class main {
-
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	static void addName() {
+		
+	}
+	
+	static void addMoney() {
+		
+	}
+	
+	static void updateMoney() {
+		
+	}
+	
+	static void deleteMoney() {
+		
+	}
+	
+	static void getAll() {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
