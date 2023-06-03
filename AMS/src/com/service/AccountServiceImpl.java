8b185a85231693455acdeed069f551b3b04b7cbc
neelayam kandel <neelayamdottkandel@gmail.com>
2023-06-03 13:27:32 +0545

    add: implementation class of account interface

---
+++ AMS/src/com/service/AccountServiceImpl.java
@@ -0,0 +1,35 @@
+package com.service;
+
+public class AccountServiceImpl implements AccountService {
+
+	@Override
+	public void AddUser() {
+		
+		
+	}
+
+	@Override
+	public void AddAmount() {
+	
+		
+	}
+
+	@Override
+	public void UpdateAmount() {
+	
+		
+	}
+
+	@Override
+	public void DeleteAmount() {
+		
+		
+	}
+
+	@Override
+	public void GetUser() {
+		
+		
+	}
+
+}
