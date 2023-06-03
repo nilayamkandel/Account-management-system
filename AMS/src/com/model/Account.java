65ffde51960b103ce31c0e6d7a0ec85ccb25c237
neelayam kandel <neelayamdottkandel@gmail.com>
2023-06-03 13:19:12 +0545

    add: pojo, constructor

--- AMS/src/com/model/Account.java
+++ AMS/src/com/model/Account.java
@@ -1,5 +1,31 @@
 package com.model;
 
 public class Account {
-
+	private int id;
+	private String userName;
+	private int accountNo;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public int getAccountNo() {
+		return accountNo;
+	}
+	public void setAccountNo(int accountNo) {
+		this.accountNo = accountNo;
+	}
+	public Account(int id, String userName, int accountNo) {
+		super();
+		this.id = id;
+		this.userName = userName;
+		this.accountNo = accountNo;
+	}
 }
