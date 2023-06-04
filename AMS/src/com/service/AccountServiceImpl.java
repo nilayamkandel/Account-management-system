dcc906395827da6f15305181e8b9534ba7cb9b51
neelayam kandel <neelayamdottkandel@gmail.com>
2023-06-04 23:04:58 +0545

    add : jdbc in impl class for delte method

--- AMS/src/com/service/AccountServiceImpl.java
+++ AMS/src/com/service/AccountServiceImpl.java
@@ -38,7 +38,18 @@ public class AccountServiceImpl implements AccountService {
 
 	@Override
 	public void DeleteAmount(int index) {
-		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/account", "root", "");
+
+			String sql = "delete from useraccount where id = 1";
+			Statement stm = con.createStatement();
+			stm.execute(sql);		
+			System.out.println("delete success");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 
